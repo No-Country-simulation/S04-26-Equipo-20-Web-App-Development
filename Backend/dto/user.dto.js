be2863fd4949b90c97e29b02f665394f54428388
unknown <duban41993@gmail.com>
2026-05-01 20:15:25 -0500

    1

---
+++ Backend/dto/user.dto.js
@@ -0,0 +1,8 @@
+export class CreateDTO {
+    constructor({emal,nombre, password,rolId}){
+        this.email = email
+        this.nombre = nombre
+        this.password = password
+        this.rolId = Number(rolId)
+    }
+}
