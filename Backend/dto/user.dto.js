5b654208137fe4fb9d02aee1d1cccb66dd530a64
duban4 <75090589+duban4@users.noreply.github.com>
2026-05-07 16:03:57 -0500

    Delete Backend/dto/user.dto.js

--- Backend/dto/user.dto.js
+++
@@ -1,8 +0,0 @@
-export class CreateDTO {
-    constructor({email,nombre, password,rolId}){
-        this.email = email
-        this.nombre = nombre
-        this.password = password
-        this.rolId = Number(rolId)
-    }
-}
