70cd613be6b2f28bd4c5eb21537816404c10e84d
spimentel1201 <spimentelm1201@gmail.com>
2026-05-11 16:02:46 -0500

    fix dependecias y prisma

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/client.ts'; // Ruta relativa al archivo generado
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 import * as dotenv from 'dotenv';
@@ -8,4 +8,4 @@ dotenv.config();
 const pool = new pg.Pool({ connectionString: process.env.DB_URL });
 const adapter = new PrismaPg(pool);
 
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter });
